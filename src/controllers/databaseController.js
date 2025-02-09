47a23eaab6f8a1a9b6f5e3e016a8114ace980d62
Daniel <danielfemartins@gmail.com>
2025-02-09 16:31:50 -0300

    func import add

--- src/controllers/databaseController.js
+++ src/controllers/databaseController.js
@@ -1,5 +1,5 @@
 import sgMail from '@sendgrid/mail';
-import { pool, loginUser, updatePassword, getTables, getDriverInfoByEmail, getPassengerInfoByEmail, getImagePathByUser, getUsersByDriverID, updatePay, getInviteUsersByDriverID, addUserEmailInvite, getUserType, addPassenger, getDriverByCode, addUser, getRaceInfoByEmail, getDriversByEmail, changeRaceStatus, getMessages, saveMessage, addCalendario, getCalendario, updateCalendario, addMotorista,getDriverRoutes, getMaxPassageirosById  } from '../services/database.js';
+import { pool, loginUser, updatePassword, getTables,getPassengerInfoById,getDriverInfoByEmail, getPassengerInfoByEmail, getImagePathByUser, getUsersByDriverID, updatePay, getInviteUsersByDriverID, addUserEmailInvite, getUserType, addPassenger, getDriverByCode, addUser, getRaceInfoByEmail, getDriversByEmail, changeRaceStatus, getMessages, saveMessage, addCalendario, getCalendario, updateCalendario, addMotorista,getDriverRoutes, getMaxPassageirosById  } from '../services/database.js';
 
 
 
